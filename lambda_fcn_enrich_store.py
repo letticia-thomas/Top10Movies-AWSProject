3555573f7410c7322d85e628d1f0b4ebe294deb5
Niwin <thomasletticia@gmail.com>
2022-05-25 00:58:10 +0530

    modular approach

--- lambda_fcn_enrich_store.py
+++ lambda_fcn_enrich_store.py
@@ -1,17 +1,19 @@
+from asyncio.windows_events import NULL
 import json
+from operator import ge
+from webbrowser import get
 import boto3
 import urllib3
 
-# creating boto3 client for accessing s3 bucket
-s3_client = boto3.client('s3')
-# creating boto3 client for accessing SQS
-sqs = boto3.client('sqs',endpoint_url = 'https://sqs.eu-central-1.amazonaws.com')
+def get_more_imdb_info(imdb_id):
+	http = urllib3.PoolManager()
+	api_url = "http://www.omdbapi.com/?i="+imdb_id+"&apikey=d5b71946"
+	response_raw = http.request('GET', api_url)
+	response_formatted = response_raw.data.decode("utf-8")
+	return(response_formatted)
 
-# This lambda function will get triggered when message is updated in SQS queue
-
-def lambda_handler(event, context):
-   
-    # retrieving message from event
+def get_movie_ids_from_SQS_event(event):
+	# retrieving message from event
 	records = event['Records']
 	message_info = records[0]
 	message_attribute = message_info['messageAttributes']
@@ -19,22 +21,30 @@ def lambda_handler(event, context):
 	movie_ids = movies['stringValue']
 
     # fetching movie id from Message arguments
-	movie_id_list = list(movie_ids.split("-"))
+	return(list(movie_ids.split("-")))
 
-    # getting more information using imDb API
-	http = urllib3.PoolManager()
-	top_movie_list =[]
+def enrich_movie_list(movie_id_list):
+	# getting more information using imDb API
+	top_movie_list = []
 	for movie_id in movie_id_list:
 		if (movie_id != ''):
-			api_url = "http://www.omdbapi.com/?i="+movie_id+"&apikey=d5b71946"
-			r = http.request('GET', api_url)
-			response = r.data.decode("utf-8")
+			response = get_more_imdb_info(movie_id)
 			response_load = json.loads(response)
 			top_movie_list.append(response_load)
-	top_movie_dictionary = {'items' : top_movie_list}
-	result_file = json.dumps(top_movie_dictionary)
+	return({'items' : top_movie_list})
 
-    # S3 bucket for storing result file
+def store_data_in_S3_bucket(enriched_data):
+	# creating boto3 client for accessing s3 bucket
+	s3_client = boto3.client('s3')
+	# S3 bucket for storing result file
 	s3_bucket = 'resultmoviebucket'
-	s3_client.put_object(Bucket = s3_bucket, Key = 'Top10Movies.json', Body = result_file)
+	s3_client.put_object(Bucket = s3_bucket, Key = 'Top10Movies.json', Body = json.dumps(enriched_data))
+
+# This lambda function will get triggered when message is updated in SQS queue
+
+def lambda_handler(event, context):
+	
+	movie_id_list = get_movie_ids_from_SQS_event(event)
+	enriched_data = enrich_movie_list(movie_id_list)
+	store_data_in_S3_bucket(enriched_data)
 	print('file updated')
